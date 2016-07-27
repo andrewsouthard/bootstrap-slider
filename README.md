3d514be154d27cd115abf2f47a6eee53656f843c
Andrew Southard <andrewmsouthard@gmail.com>
2016-07-27 17:39:24 -0400

    Added per slider initialization options.

--- README.md
+++ README.md
@@ -1,7 +1,34 @@
 # bootstrap-slider
 Multi-item bootstrap slider based on the bootstrap carousel written using jQuery. Take the bootstrap carousel example code from the [bootstrap website](http://getbootstrap.com/javascript/#carousel) and add the 
-slider CSS class and include slider.css and slider.js. The slider.js module will find all sliders on the page and initialize them for you. When including the script, you can add the following options to the script tag to override the defaults:
+slider CSS class and include slider.css and slider.js. The slider.js module will find all sliders on the page and initialize them for you. When including the script, you can use the following options in the script tag to override the defaults by prepending '_data-_':
 
-* **data-activeItems**: A number specifying how many items should be displayed. The default is three.
-* **data-indicatorLinks**: true or false. This defines whether to make the indicators links that change the slider to the item associated with that indicator.
-* **data-itemInFocus**: A number specifying which item of the active items should be in focus. The default is the first item.
+* __activeItems__: A number specifying how many items should be displayed. The default is three.
+* __indicatorLinks__: True or false. This defines whether to make the indicators links that change the slider to the item associated with that indicator.
+* __itemInFocus__: A number specifying which item of the active items should be in focus. The default is the first item.
+* __manualInit*__: True or false. Have all slider on the page automatically initialized or tell the module it will be done manually. This is usually used to specify different settings for different sliders. The default is false.
+
+When the manualInit option is used, each slider on the page must be manually initialized. This is done in a second tag by calling _slider.init(sliderID,options)_. The _sliderID_ is the id of the div that has the slider class. The options  value is an object with names set to any of the options in the list above except those with an asterisk. 
+
+For example:
+```html
+<div id="myslider1" class="slider ...">
+...
+</div>
+<div id="myslider2" class="slider ...">
+...
+</div>
+<script type="text/javascript" src="slider.js" data-manualInit="true"></script>
+<script type="text/javascript">
+    slider.init('myslider1',{'activeItems': 2});
+    slider.init('myslider2');
+</script>
+```
+
+Within the slider, each item can also be a link to another page. To specify where each item should link to when clicked, add a _data-href_ tag to the item. For example:
+```html
+<div class="item" data-href="http://example.com">
+...
+</div>
+```
+
+There are two example html files included in the repo to see the slider in action. 
--- slider.js
+++ slider.js
@@ -1,28 +1,67 @@
-(function() {
+var slider = (function() {
     /* Setup some defaults */
     var windowTransitionSize = 1028;
-    var indicatorLinks = true;
-    var itemInFocus = 1;
-    var numActiveItems = 3;
     var winSize = windowSize();
+    var manualInit = false;
 
-    /* Set any user defined values. */
+    /* Options that can only be set when the src call is made via 'data-'
+     * options. */
+    var dataOnlyOptions = {
+        'manualInit': 'data-manualInit',
+    };
+    var validOptions = {
+        'numActiveItems': 'data-activeItems',
+        'indicatorLinks': 'data-indicatorLinks',
+        'itemInFocus': 'data-itemInFocus',
+    };
+    var options = {
+        'default': {
+            'indicatorLinks': true,
+            'itemInFocus': 1,
+            'numActiveItems':  3,
+        },
+    };
+
+    /* Parse the script params and set the defaults. */
     getScriptParams();
 
     /* Initialize each slider on the page once the page finishes loading. */
     $(document).ready(function() {
-        var list = $(".slider");
-        if($(list).length > 1) {
-            $(list).each(function(){
-                changeSlider(0,$(this).attr('id'));
-            });
-        } else {
-            changeSlider(0);
+        if(! manualInit) {
+            var list = $(".slider");
+            if($(list).length > 1) {
+                $(list).each(function(){
+                    initializeSlider($(this).attr('id'));
+                });
+            } else {
+                initializeSlider($($('.slider')[0]).attr('id'));
+            }
         }
     });
 
-    /* Change the number of visible items in the slider on 
-     * window resize if  necessary */
+    /* Initialize a slider by setting up default values, displaying the slider,
+     * and adding click listeners for each control method. */
+    function initializeSlider(id,opts) {
+        /* An ID must be provided. Return here if it isn't. */
+        if(! id) return;
+
+        /* Set options for this slider. If no options or invalid options are
+         * provided, use the defaults. */
+        setOptions(id,opts);
+
+        /* Setup the slider */
+        changeSlider(0,id);
+
+        /* Create click listeners for the slider controls, each item in the
+         * slider and optionally for the indicators. */
+        controlsListeners(id);
+        itemListener(id);
+        if(options[id].indicatorLinks) {
+            addIndicatorListeners(id);
+        }
+    }
+    /* Change the number of visible items in the slider on
+     * window resize if necessary */
     window.onresize = function() {
         var newWinSize = windowSize();
         if(newWinSize != winSize) {
@@ -40,115 +79,154 @@
 
     /* Parse input and override defaults if the caller specifies values */
     function getScriptParams() {
+        var scriptOpts = {};
         var scripts = document.getElementsByTagName('script');
         var scriptName  = scripts[scripts.length-1];
-        var validOptions = {
-            'data-activeItems': 'numActiveItems',
-            'data-indicatorLinks': 'indicatorLinks',
-            'data-itemInFocus': 'itemInFocus',
-        };
-        for(option in validOptions) {
-            var attr = $(scriptName).attr(option);
+
+        /* Override any defaults for values that can only be set globally via
+         * the 'data-' calls and not on a per slider basis. */
+        for(opt in dataOnlyOptions) {
+            var attr = $(scriptName).attr(dataOnlyOptions[opt]);
+            if (typeof attr !== typeof undefined && attr !== false) {
+                    if(attr === 'true') {
+                        eval(opt+'='+true);
+                    } else {
+                        eval(opt+'='+false);
+                    }
+            }
+        }
+
+        /* Define each option that can be called via the 'data-' calls or at
+         * slider initialization in order to setup the default values. */
+        for(opt in validOptions) {
+            scriptOpts[opt] = $(scriptName).attr(validOptions[opt]);
+        }
+        setOptions('default',scriptOpts);
+    }
+
+    function setOptions(id,opts) {
+        if(! id) return;
+        var myOpts = opts || {};
+
+        /* Initialize the options object entry for this id if it isn't already
+         * defined. */
+        if(! options[id]) {
+            options[id] = {};
+        }
+
+        /* Cycle through each valid option and see if it has been overridden by
+         * a user specified value */
+        for(opt in validOptions) {
+            var attr = myOpts[opt];
             if (typeof attr !== typeof undefined && attr !== false) {
                 if(attr === 'false') {
-                    eval(validOptions[option]+'='+ false);
+                    options[id][opt] = false;
                 } else {
-                    if(validOptions[option] == 'numActiveItems') {
+                    if(opt == 'numActiveItems') {
                         if(! Number.isInteger(parseInt(attr))) {
                             attr = 3;
+                        } else {
+                            attr = parseInt(attr);
                         }
-                    } else if(validOptions[option] == 'itemInFocus') {
-                        if(! Number.isInteger(parseInt(attr)) || attr > numActiveItems || attr < 1) {
+                    } else if(opt == 'itemInFocus') {
+                        if(! Number.isInteger(parseInt(attr)) || attr > options[id].numActiveItems || attr < 1) {
                             attr = 1;
+                        } else {
+                            attr = parseInt(attr);
                         }
                     }
-                    eval(validOptions[option]+'='+ attr);
+                    options[id][opt]= attr;
                 }
+            } else {
+                /* Use the default if another value isn't specified. */
+                options[id][opt] = options['default'][opt];
             }
         }
     }
 
     /* Setup a click listener for slider controls.*/
-    $('.carousel-control').click(function(event) {
-        /* Get the slider ID to use to change the item in focus */
-        var sliderID = $($(this).parents('.slider')[0]).attr('id');
+    function controlsListeners(sliderID) {
+        $('#'+sliderID+' .carousel-control').click(function(event) {
+            /* Nothing will happen unless the control has a left or right class. It
+             * should based on the default bootstrap carousel layout. */
+            if($(this).hasClass('left')) {
+                changeSlider('-',sliderID);
+            } else if($(this).hasClass('right')) {
+                changeSlider('+',sliderID);
+            }
+        });
+    }
 
-        /* Nothing will happen unless the control has a left or right class. It 
-         * should based on the default bootstrap carousel layout. */
-        if($(this).hasClass('left')) {
-            changeSlider('-',sliderID); 
-        } else if($(this).hasClass('right')) {
-            changeSlider('+',sliderID); 
-        }
-    });
     /* Setup a click listener for each item so if a user clicks on it, it 
      * becomes the item in focus. */
-    $('.carousel-inner').on('click','.item',function(event) { 
-        var openLinkDelay = 600; 
+    function itemListener(sliderID) {
+        $('#'+sliderID+' .carousel-inner').on('click','.item',function(event) {
+            var openLinkDelay = 600;
 
-        /* Get the slider ID to use to change the item in focus */
-        var sliderID = $($(this).parents('.slider')[0]).attr('id');
+            /* Determine where in the list of items we are */
+            var index = $(this).index();
 
-        /* Determine where in the list of items we are */
-        var index = $(this).index();
+            /* Determine the number of items and the number of items that are
+             * permanent. These may be different because temporary items are
+             * added to the list so the slider will appear continuous. */
+            var len = $('#'+sliderID+' .carousel-inner .item').length;
+            var absLen = $('#'+sliderID+' .carousel-inner .item').not('.start-temp').not('.end-temp').length;
 
-        /* Determine the number of items and the number of items that are 
-         * permanent. These may be different because temporary items are 
-         * added to the list so the slider will appear continuous. */
-        var len = $('#'+sliderID+' .carousel-inner .item').length;
-        var absLen = $('#'+sliderID+' .carousel-inner .item').not('.start-temp').not('.end-temp').length;
+            /* If the absolute length is different than the current length (i.e.
+             * if there are temporary items right now) */
+            if(absLen < len) {
+                /* Get the number of items that were temporarily added to the end. We
+                 * need this to adjust the index. */
+                var numEndTemp = len - $('#'+sliderID+' .carousel-inner .item').not('.start-temp').length;
 
-        /* If the absolute length is different than the current length (i.e. 
-         * if there are temporary items right now) */
-        if(absLen < len) {
-            /* Get the number of items that were temporarily added to the end. We 
-             * need this to adjust the index. */
-            var numEndTemp = len - $('#'+sliderID+' .carousel-inner .item').not('.start-temp').length;
+                /* Adjust the index location if there were extra items added to 
+                 * the end.*/
+                index = index - (numEndTemp);
 
-            /* Adjust the index location if there were extra items added to 
-             * the end.*/
-            index = index - (numEndTemp);
-
-            /* Update the index to reflect the actual location of the item if a
-             * temporary item was clicked on. */
-            if($(this).hasClass('start-temp')) {
-                index = len - numEndTemp  + index;
+                /* Update the index to reflect the actual location of the item if a
+                 * temporary item was clicked on. */
+                if($(this).hasClass('start-temp')) {
+                    index = len - numEndTemp  + index;
+                }
+                else if($(this).hasClass('end-temp')) {
+                    index = index - absLen;
+                }
             }
-            else if($(this).hasClass('end-temp')) {
-                index = index - absLen;
+
+            /* If the item has a link defined, set the url variable so it can be followed */
+            if($(this).attr('data-href') != null) {
+                var url = $(this).attr('data-href');
+                if($(this).hasClass('focused')) {
+                    openLinkDelay = 0;
+                }
             }
-        } 
 
-        /* If the item has a link defined, set the url variable so it can be followed */
-        if($(this).attr('data-href') != null) {
-            var url = $(this).attr('data-href');
-            if($(this).hasClass('focused')) {
-               openLinkDelay = 0;
+            /* Update the item in focus */
+            changeSlider(index,sliderID);
+
+            /* If the item has a link attached to it, follow it. If the item wasn't
+             * previously in focus, follow the link after a short delay to allow
+             * the item to come into focus. */
+            if(url) {
+                setTimeout(function(){
+                    window.open(url,'_target=blank');
+                },openLinkDelay);
             }
-        }
-
-        /* Update the item in focus */
-        changeSlider(index,sliderID);
-
-        /* If the item has a link attached to it, follow it. If the item wasn't
-         * previously in focus, follow the link after a short delay to allow 
-         * the item to come into focus. */
-        if(url) {
-            setTimeout(function(){
-            window.open(url,'_target=blank');
-            },openLinkDelay);
-        }
-
-    });
+        });
+    }
 
     /* Add a click listener for indicators */
-    $('.carousel-indicators li').click(function(event) { 
-        if(indicatorLinks) {
-            /* Get the slider ID to use to change the item in focus */
-            var sliderID = $($(this).parents('.slider')[0]).attr('id');
-            changeSlider( $(this).index(),sliderID); 
+    function addIndicatorListeners(id) {
+        if(id) {
+            var sliderID = id;
+        } else {
+            var sliderID = $($('.slider')[0]).attr('id');
         }
-    });
+        $('#'+sliderID+' .carousel-indicators li').click(function(event) {
+            /* Get the slider ID to use to change the item in focus */
+            changeSlider($(this).index(),sliderID);
+        });
+    }
     /* Return 1 if the window size is larger than the transition size, 
      * 0 otherwise. */
     function windowSize() {
@@ -157,20 +235,13 @@
 
     /* Slider javascript */ 
     function changeSlider(num,sliderID) {
-        var inner      = '.carousel-inner';
-        var sliderItem = inner+' .item';
-        var indicators = '.carousel-indicators';
-        var controls   = '.carousel-control';
-        var overlay       = '.grey-img-overlay';
+        /* Only change the slider if the number and slider are defined. */
+        if(num == null || sliderID == null) return;
 
-        /* Prepend the slider ID. This is used if there is more than one slider
-         * per page. Otherwise the variable will be null. */
-        if(sliderID != null) {
-            sliderItem    = '#'+sliderID+' '+sliderItem;
-            controls      = '#'+sliderID+' '+controls;
-            indicators    = '#'+sliderID+' '+indicators;
-            inner         = '#'+sliderID+' '+inner;
-        }
+        var sliderItem = '#'+sliderID+' .item';
+        var controls   = '#'+sliderID+' .carousel-control';
+        var indicators = '#'+sliderID+' .carousel-indicators';
+        var inner      = '#'+sliderID+' .carousel-inner';
 
         /* Define the focused item for future use */
         var activeItem = sliderItem+'.active.focused';
@@ -178,12 +249,12 @@
         /* Get the number of items. */
         var len = $(sliderItem).not('.start-temp').not('.end-temp').length;
 
-        // Define the offset
-        var offset = function() { 
-            if(len < numActiveItems) {
+        /* Define the offset */
+        var offset = function() {
+            if(len < options[sliderID].numActiveItems) {
                 return 0;
             } else {
-                return 1 - itemInFocus; 
+                return 1 - options[sliderID].itemInFocus;
             }
         };
 
@@ -192,20 +263,12 @@
         $(sliderItem).remove('.end-temp');
 
         /* Set the active slide number if only told to increment/decrement. Have to
-         * do this here before the focused class is removed from the previously 
+         * do this here before the focused class is removed from the previously
          * selected element. */
         if(num == '-') {
             num = $(activeItem).index() -1;
         } else if(num == '+') {
             num = $(activeItem).index() +1;
-        }
-
-        /* Reset the onclick link for the previous slider item  */
-        var activeHref = $(activeItem).children('a').attr('activehref');
-        if(activeHref != null) {
-            $(activeItem).children('a').attr('activehref',$(activeItem).children('a').attr('href'));
-            $(activeItem).children('a').attr('onclick',activeHref);
-            $(activeItem).children('a').attr('href','javascript:void(0)');
         }
 
         /* Reset the style for the previous slider item */
@@ -223,10 +286,6 @@
             $(controls).hide(); 
         }
 
-        if(len < numActiveItems) {
-            $(inner).addClass("carousel-left");
-        }
-
         /* Set the correct indicator to active */
         $(indicators).find('li').removeClass('active');
         $(indicators).find('li').eq(num).addClass('active');
@@ -237,7 +296,7 @@
         /* If the window size is greater than or equal to windowTransitionSize, activate the correct number of items */
         if($( window ).width() >= windowTransitionSize) {
             var prepend = [];
-            for(var i = offset(); i < numActiveItems + offset() ; i++) {
+            for(var i = offset(); i < options[sliderID].numActiveItems + offset() ; i++) {
                 var imgNum = (num + i);
                 if(i >= len) {
                     break;
@@ -248,13 +307,13 @@
                     $(sliderItem).not('.start-temp').eq(imgNum).addClass('active');
                     if(prepend.length > 0) {
                         prepend.reverse();
-                        $(prepend).each(function(index) { 
+                        $(prepend).each(function(index) {
                             $(prepend[index]).prependTo(inner);
                         });
                         prepend = [];
                     }
                 } else if(imgNum == len) {
-                    if(len > numActiveItems -1) {
+                    if(len > options[sliderID].numActiveItems -1) {
                         $(sliderItem).eq(imgNum % len).clone().appendTo(inner);
                         $(sliderItem).eq($(sliderItem).length - 1).addClass('end-temp active item');
                     } else {
@@ -271,16 +330,11 @@
         } else {
             $(sliderItem).eq(num).addClass('active');
         }
+
         /* Add the focused class to the current item. */
         $(sliderItem).not('.start-temp').eq(num).addClass('focused');
-
-        /* If the activehref key is used, use it to change out the link on the 
-         * focused item from making the item focused, to another page. */
-        var activeHref = $(sliderItem).eq(num).children('a').attr('activehref');
-        if(activeHref != null) {
-            $(sliderItem).eq(num).children('a').attr('activehref',$(sliderItem).eq(num).children('a').attr('onclick'));
-            $(sliderItem).eq(num).children('a').attr('href',activeHref);
-            $(sliderItem).eq(num).children('a').attr('onclick','');
-        }
     }
+    /* Make the initialize function publicly available via the module.init
+     * call. */
+    return { init: initializeSlider };
 })();
